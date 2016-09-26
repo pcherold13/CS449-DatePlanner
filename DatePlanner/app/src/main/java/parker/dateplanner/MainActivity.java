ab35f006f96e53cc4d710b28af3a6f7492b52eba
pcherold13 <pcherold13@gmail.com>
2016-09-26 14:45:48 -0500

    Dating App
    Commit

---
+++ DatePlanner/app/src/main/java/parker/dateplanner/MainActivity.java
@@ -0,0 +1,59 @@
+package parker.dateplanner;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.reset:
+                String blank = "";
+                TextView dinner = (TextView) findViewById(R.id.dinnerText);
+                TextView movie = (TextView) findViewById(R.id.movieText);
+                TextView location = (TextView) findViewById(R.id.locationText);
+                dinner.setText(blank);
+                movie.setText(blank);
+                location.setText(blank);
+                return true;
+            case R.id.about:
+                Intent about = new Intent(this, AboutActivity.class);
+                startActivity(about);
+                return true;
+            case R.id.settings:
+                Intent settings = new Intent(this, SettingsActivity.class);
+                startActivity(settings);
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    String search = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+
+    private void search (String dinner, String movie, String location){
+        search += dinner + " and " + movie + " showing near " + "location";
+    }
+}
---
+++ DatePlanner/app/src/main/java/parker/dateplanner/SettingsActivity.java
@@ -0,0 +1,22 @@
+package parker.dateplanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+     /*   View incrementBall = findViewById(R.id.Submit_Button);
+        incrementBall.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+    */
+    }
+}
