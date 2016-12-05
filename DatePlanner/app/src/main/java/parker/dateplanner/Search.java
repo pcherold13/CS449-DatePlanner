db252eddaaceee6fd2f0b1d017f5e569cc654cd1
pcherold13 <pcherold13@gmail.com>
2016-12-05 14:14:54 -0600

    Update Search.java

--- DatePlanner/app/src/main/java/parker/dateplanner/Search.java
+++ DatePlanner/app/src/main/java/parker/dateplanner/Search.java
@@ -1,14 +1,31 @@
 package parker.dateplanner;
 
-public class Search{
-  public void start(TextView dinner, TextView movie, boolean gps)
-      String place; -       
- -    if(gps){
- -        place = GPS.locate();                        
- -    }
- -    else{
- -        TextView location = (TextView) findViewById(R.id.locationText);
- -        place = location.getText();
- -    }
- -    search += dinner.getText() + " and " + movie.getText() + " showing near " + place;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.util.Log;
+import android.widget.Toast;
+
+public class Search {
+    public void start(TextView dinner, TextView movie, boolean gps) {
+        String place, search;
+        if (gps) {
+            place = GPS.locate();
+        } else {
+            TextView location = (TextView) findViewById(R.id.locationText);
+            place += location.getText();
+        }
+        search += dinner.getText() + " and " + movie.getText() + " showing near " + place;
+    }
 }
