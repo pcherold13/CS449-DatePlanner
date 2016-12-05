4b0c7995da1a3a9133c70f59f0d877d033b7c99a
pcherold13 <pcherold13@gmail.com>
2016-12-05 07:59:07 -0600

    Update Search.java

--- DatePlanner/app/src/main/java/parker/dateplanner/Search.java
+++ DatePlanner/app/src/main/java/parker/dateplanner/Search.java
@@ -2,6 +2,13 @@ package parker.dateplanner;
 
 public class Search{
   public void start(TextView dinner, TextView movie, boolean gps)
-      
-
+      String place; -       
+ -    if(gps){
+ -        place = GPS.locate();                        
+ -    }
+ -    else{
+ -        TextView location = (TextView) findViewById(R.id.locationText);
+ -        place = location.getText();
+ -    }
+ -    search += dinner.getText() + " and " + movie.getText() + " showing near " + place;
 }
